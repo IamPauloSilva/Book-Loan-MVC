7644265c4030243e803ff1de0d255850efd80cea
Paulo Silva <timmylaggy.as@gmail.com>
2024-08-12 04:35:31 +0100

    added auto migration

--- Program.cs
+++ Program.cs
@@ -42,6 +42,11 @@ builder.Services.AddSession(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
